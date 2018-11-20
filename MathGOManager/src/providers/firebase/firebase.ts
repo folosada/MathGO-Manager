5762b91c6baec81a90b4c51d3997ab21a992bf28
Flávio Omar Losada <flavio_gs96@hotmail.com>
2018-11-20 06:08:08 -0200

    firebase

--- MathGOManager/src/providers/firebase/firebase.ts
+++ MathGOManager/src/providers/firebase/firebase.ts
@@ -9,7 +9,15 @@ export class FirebaseProvider {
 
   constructor(private db: AngularFireDatabase) {
   }
-  
+
+  getAll() {
+    return this.db.list(this.PATH, ref => ref.orderByChild('name'))
+      .snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.key, ...c.payload.val() }));
+      })
+  }
+
   get(key: string) {
     return this.db.object(this.PATH + key).snapshotChanges()
       .map(c => {
